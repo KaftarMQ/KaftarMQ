cd63f301f3f5ff638909886cf57b226061e367d2
amirali <ebrahimzadeh.amirali@gmail.com>
2023-12-30 15:57:37 +0330

    Basic skeleton of producer completed.

---
+++ Producer/MessageGenerator.cs
@@ -0,0 +1,25 @@
+﻿using Consumer;
+
+namespace Producer;
+
+public class MessageGenerator
+{
+    public static async Task<Message> generate()
+    {
+        Message message = new Message();
+        message.MessageId = generateKey();
+        message.Content = generateValue();
+        return message;
+    }
+
+    private static string generateKey()
+    {
+        throw new NotImplementedException();
+    }
+    
+    private static string generateValue()
+    {
+        throw new NotImplementedException();
+    }
+
+}
