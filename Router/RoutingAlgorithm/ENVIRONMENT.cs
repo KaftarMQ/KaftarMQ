54ceff0ec1cc6a04b4cb7e8639225893935a56c2
Amirhossein Arabzadeh <arabzadehamirhossein888@gmail.com>
2024-02-10 16:03:22 +0330

    add routing algorithm to router and syncer

---
+++ Router/RoutingAlgorithm/ENVIRONMENT.cs
@@ -0,0 +1,8 @@
+﻿namespace RoutingAlgorithm;
+
+public static class ENVIRONMENT
+{
+    public static List<string> ALL_BROKERS = new() { "http://broker"};
+    public static List<string> ALL_ROUTERS = new() { "http://router"};
+    public static string NGINX = "http://nginx";
+}
---
+++ Syncer/RoutingAlgorithm/BrokerData.cs
@@ -0,0 +1,26 @@
+﻿namespace Syncer;
+
+public class BrokerData
+{
+    public BrokerData(string Url, bool IsFailed)
+    {
+        this.Url = Url;
+        this.IsFailed = IsFailed;
+    }
+
+    public string Url { get; init; }
+    public bool IsFailed { get; set; }
+
+    public sealed class UrlRelationalComparer : IComparer<BrokerData>
+    {
+        public int Compare(BrokerData x, BrokerData y)
+        {
+            if (ReferenceEquals(x, y)) return 0;
+            if (ReferenceEquals(null, y)) return 1;
+            if (ReferenceEquals(null, x)) return -1;
+            return string.Compare(x.Url, y.Url, StringComparison.Ordinal);
+        }
+    }
+
+    public static IComparer<BrokerData> UrlComparer { get; } = new UrlRelationalComparer();
+}
