553ae33add1ec0404222cc03cd2711fc4b8a10e2
ghalandary <ab.ghalandary@outlook.com>
2024-02-11 21:13:14 +0330

    s

--- Client2/Program.cs
+++ Client2/Program.cs
@@ -1,19 +1,19 @@
 ﻿using System.Text;
 using ClientLibrary;
 
-API.Push("Khalafi Eshgh 1", "Message 1"u8.ToArray());
-API.Push("Khalafi Eshgh 1", "Message 2"u8.ToArray());
-API.Push("Khalafi Eshgh 2", "Message 1"u8.ToArray());
-API.Push("Khalafi Eshgh 2", "Message 2"u8.ToArray());
+API.Push("Khalafi Eshgh 4", "Message 1"u8.ToArray());
+API.Push("Khalafi Eshgh 5", "Message 2"u8.ToArray());
+API.Push("Khalafi Eshgh 6", "Message 1"u8.ToArray());
+API.Push("Khalafi Eshgh 7", "Message 2"u8.ToArray());
 
 Action<(string key, byte[] value)> f = message =>
 {
     var value = Encoding.UTF8.GetString(message.value);
     Console.WriteLine($"Received message with key: \"{message.key}\", value: \"{value}\"");
 };
-
-f(API.Pull());
-
-API.Subscribe(f);
-
-Thread.Sleep(TimeSpan.FromMinutes(5));
+//
+// f(API.Pull());
+//
+// API.Subscribe(f);
+//
+// Thread.Sleep(TimeSpan.FromMinutes(5));
